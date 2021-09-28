97c0bbe8f01e23d1c34ee55a4bb9dace5e043311
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-28 20:00:50 +0530

    add: added deploy function for deploy token

---
+++ scripts/deploy_token.py
@@ -0,0 +1,15 @@
+from scripts.helper_script import get_account
+from brownie import OurToken
+from web3 import Web3
+
+initial_supply = Web3.toWei(1000, "ether")
+
+
+def deploy_token():
+    account = get_account()
+    our_token = OurToken.deploy(initial_supply, {"from": account})
+    print(our_token.name())
+
+
+def main():
+    deploy_token()
