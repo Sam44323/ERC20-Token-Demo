badcab176a9675069521acf72077c76a9299a83e
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-28 20:23:33 +0530

    add: added network contract verification on deplpy

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,5 +1,5 @@
 from scripts.helper_script import get_account
-from brownie import OurToken
+from brownie import OurToken, config, network
 from web3 import Web3
 
 initial_supply = Web3.toWei(1000, "ether")  # the initial supply value
@@ -7,8 +7,9 @@ initial_supply = Web3.toWei(1000, "ether")  # the initial supply value
 
 def deploy_token():
     account = get_account()
-    our_token = OurToken.deploy(initial_supply, {"from": account})
-    print(our_token.name())
+    our_token = OurToken.deploy(
+        initial_supply, {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False))
 
 
 def main():
